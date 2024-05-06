960eba5c537a9c453536fe241c5e0e75b6a0672c
repossum89 <octobersy@naver.com>
2024-05-07 01:36:28 +0900

    [Refactor] UI 초기 화면 구상

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 import './App.css';
+import MainPage from './MainPage';
+import LoginPage from './LoginPage';
+import DetailPage from './DetailPag';
 
 function App() {
+  console.log(process.env.PUBLIC_URL)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='header-master'>
+        <div className='header-container'>
+          <div className='header-name'>
+            <p>
+              <a href={process.env.PUBLIC_URL} rel='home'>SOYEON KIM</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='content-master'>
+        <ul className='content-category'>
+          <li><Link to='/portfolio/web'>web</Link></li>
+          <li><Link to='/portfolio/logo'>logo</Link></li>
+          <li>babo</li>
+          <li><Link to='/portfolio/brand'>brand</Link></li>
+        </ul>
+        <Routes>
+          <Route path="/portfolio/web" element={<MainPage />} />
+          <Route path="/portfolio/logo" element={<LoginPage />} />
+          <Route path="/portfolio/detail" element={<DetailPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
